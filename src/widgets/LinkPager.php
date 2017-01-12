602c999c6229b1ceffea6739ece9a0ba8cf94864
tofid <andreyklochok@gmail.com>
2017-01-12 19:53:47 +0200

    Added LinkPager widget

---
+++ src/widgets/LinkPager.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace hiqdev\themes\hyde\widgets;
+
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+
+class LinkPager extends \yii\widgets\LinkPager
+{
+    /** @inheritdoc */
+    public $prevPageCssClass = 'pagination-item newer';
+
+    /** @inheritdoc */
+    public $nextPageCssClass = 'pagination-item older';
+
+    /** @inheritdoc */
+    public function renderPageButtons()
+    {
+        $pageCount = $this->pagination->getPageCount();
+        if ($pageCount < 2 && $this->hideOnSinglePage) {
+            return '';
+        }
+
+        $buttons = [];
+        $currentPage = $this->pagination->getPage();
+
+        // prev page
+        if ($this->prevPageLabel !== false) {
+            if (($page = $currentPage - 1) < 0) {
+                $page = 0;
+            }
+            $buttons[] = $this->renderPageButton($this->prevPageLabel, $page, $this->prevPageCssClass, $currentPage <= 0, false);
+        }
+
+        // next page
+        if ($this->nextPageLabel !== false) {
+            if (($page = $currentPage + 1) >= $pageCount - 1) {
+                $page = $pageCount - 1;
+            }
+            $buttons[] = $this->renderPageButton($this->nextPageLabel, $page, $this->nextPageCssClass, $currentPage >= $pageCount - 1, false);
+        }
+
+        return Html::tag('div', implode("\n", $buttons), $this->options);
+    }
+
+    /** @inheritdoc */
+    protected function renderPageButton($label, $page, $class, $disabled, $active)
+    {
+        $options = ['class' => empty($class) ? $this->pageCssClass : $class];
+        if ($active) {
+            Html::addCssClass($options, $this->activePageCssClass);
+        }
+        if ($disabled) {
+            Html::addCssClass($options, $this->disabledPageCssClass);
+            $tag = ArrayHelper::remove($this->disabledListItemSubTagOptions, 'tag', 'span');
+
+            return Html::tag($tag, $label, $options);
+        }
+        $linkOptions = $this->linkOptions;
+        $linkOptions['data-page'] = $page;
+
+        return Html::a($label, $this->pagination->createUrl($page), $options);
+    }
+}
+
