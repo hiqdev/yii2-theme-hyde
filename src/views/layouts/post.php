49e136c65f9a86e9c7d6fc6306c725171d0606ee
Andrii Vasyliev <sol@hiqdev.com>
2017-01-13 17:25:33 +0000

    + post layout

---
+++ src/views/layouts/post.php
@@ -0,0 +1,27 @@
+<?php $this->beginContent('@hiqdev/themes/hyde/views/layouts/main.php') ?>
+
+<div class="post">
+    <h1 class="post-title"><?= $this->title ?></h1>
+    <span class="post-date"><?= $this->params['date'] ?></span>
+    <?= $content ?>
+</div>
+
+<?php if (!empty($related_posts)) : ?>
+    <div class="related">
+        <h2>Related Posts</h2>
+        <ul class="related-posts">
+            {% for post in site.related_posts limit:3 %}
+                <li>
+                    <h3>
+                        <a href="{{ post.url }}">
+                            {{ post.title }}
+                            <small>{{ post.date | date_to_string }}</small>
+                        </a>
+                    </h3>
+                </li>
+            {% endfor %}
+        </ul>
+    </div>
+<?php endif ?>
+
+<?php $this->endContent() ?>
