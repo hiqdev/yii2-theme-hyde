8f9b91a9ad109dde013106d6347faceb6cb93d38
tofid <andreyklochok@gmail.com>
2017-01-16 14:07:41 +0200

    Added page layout

---
+++ src/views/layouts/page.php
@@ -0,0 +1,8 @@
+<?php $this->beginContent('@hiqdev/themes/hyde/views/layouts/main.php') ?>
+
+<div class="page">
+    <h1 class="post-title"><?= $this->title ?></h1>
+    <?= $content ?>
+</div>
+
+<?php $this->endContent() ?>
