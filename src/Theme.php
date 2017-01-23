6f440b298bc613b745d4f837812159dc2144e77e
tofid <andreyklochok@gmail.com>
2017-01-23 18:47:03 +0200

    Added screenshot

--- src/Theme.php
+++ src/Theme.php
@@ -6,6 +6,8 @@ class Theme extends \hiqdev\thememanager\Theme
 {
     public $label = 'Hyde';
 
+    public $screenshot = '@hiqdev/themes/hyde/assets/hyde.png';
+
     public $assets = [
         Asset::class,
     ];
